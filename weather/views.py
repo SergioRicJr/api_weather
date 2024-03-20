bc3fe105681ea861d6064ed1d9c38603822c99f0
SergioNascimento07 <sergioricjr7@gmail.com>
2024-03-20 20:30:31 -0300

    feat: views alteradas para serializar weather

--- weather/views.py
+++ weather/views.py
@@ -4,20 +4,23 @@ from django.views import View
 from django.shortcuts import render, redirect
 from .models import WeatherEntity
 from .repositories import WeatherRepository
+from .serializers import WeatherSerializer
 
 class WeatherView(View):
     def get(self, request):
         repository = WeatherRepository(collection_name='weathers')
         weathers = repository.getAll()
-        return render(request, "home.html", {"weathers":weathers})
+        serializer = WeatherSerializer(weathers, many=True)
+        return render(request, "home.html", {"weathers": serializer.data})
 
 class WeatherGenerate(View):
     def get(self, request):
         repository = WeatherRepository(collection_name='weathers')
-        wheater = {
-            "temperature" : 28,
-            "date": "hoje"
-            }
-        repository.insert(wheater)
+        weather = WeatherEntity(
+            temperature=randrange(start=17, stop=40),
+            date=datetime.now()
+        )
+        serializer = WeatherSerializer(data=weather)
+        repository.insert(serializer.data)
 
         return redirect('Weather View')
