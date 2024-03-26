aeaa898b3d566763dc68713da0db52c4d8e5510e
SergioNascimento07 <sergioricjr7@gmail.com>
2024-03-25 22:17:28 -0300

    feat: tratamento de atributo data no weather model
    atributo data formatado para sair com padrão dia/mes/ano hora:minuto:segundos

--- weather/models.py
+++ weather/models.py
@@ -1,13 +1,11 @@
+from typing import Any
+from django.db import models
+
 class WeatherEntity:
-    def __init__(
-            self, 
-            temperature, 
-            date,
-            city='', 
-            atmosphericPressure='',
-            humidity='', 
-            weather=''
-    ) -> None:
+
+    def __init__(self, temperature, date,
+                 city='', atmosphericPressure=0,
+                 humidity=0, weather='') -> None:
         self.temperature = temperature
         self.city = city
         self.atmosphericPressure = atmosphericPressure
@@ -17,3 +15,9 @@ class WeatherEntity:
 
     def __str__(self) -> str:
         return (f"Weather <{self.temperature}>")
+    
+    def __getattribute__(self, __name: str) -> Any:
+        if (__name=='date'):
+            return object.__getattribute__(self, __name).strftime("%d/%m/%Y %H:%M:%S")
+        else:
+            return object.__getattribute__(self, __name)
