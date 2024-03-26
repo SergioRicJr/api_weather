5646a6a5c6d531a6f3f46dafd1ba4d0c2cf4d067
SergioNascimento07 <sergioricjr7@gmail.com>
2024-03-25 23:11:43 -0300

    chore: função de deletar card adicionada as urls

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
-from weather.views import WeatherView, WeatherGenerate, WeatherReset, WeatherInsert
+from weather.views import (
+    WeatherDeleteView,
+    WeatherView,
+    WeatherGenerate,
+    WeatherReset,
+    WeatherInsert,
+)
 
 urlpatterns = [
     path("", WeatherView.as_view(), name="Weather View"),
     path("insert", WeatherInsert.as_view(), name="Weather Insert"),
     path("generate", WeatherGenerate.as_view(), name="Weather Generate"),
     path("reset", WeatherReset.as_view(), name="Weather Reset"),
+    path("weather/delete/", WeatherDeleteView.as_view(), name="delete_weather"),
 ]
